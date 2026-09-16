839d5ee3a9e2136e678a1b9c0cd62c36d6fee7e2
Maksim Sobolev <maksim.sobolev@cadexchanger.com>
2026-09-16 12:01:48 +0300

    added drawio and md notes, reduce code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,5 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
-import { getUsers } from "@/services/user";
-
 export default function Home() {
-	const usersQuery = useQuery({
-		queryKey: ["users"],
-		queryFn: () => getUsers({}),
-	});
-
-	if (usersQuery.isPending) {
-		return <main>Loading...</main>;
-	}
-
-	if (usersQuery.isError) {
-		return <main>Failed to load users</main>;
-	}
-
-	return (
-		<main>
-			<h1>Users</h1>
-
-			<table>
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Email</th>
-						<th>Status</th>
-						<th>Registered</th>
-					</tr>
-				</thead>
-
-				<tbody>
-					{usersQuery.data.items.map((user) => (
-						<tr key={user.id}>
-							<td>{user.name}</td>
-							<td>{user.email}</td>
-							<td>{user.status}</td>
-							<td>{user.registeredAt}</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</main>
-	);
+	return <main></main>;
 }
